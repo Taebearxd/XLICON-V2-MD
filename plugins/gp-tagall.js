b2165a5d0ba239533ce4ec95b2d23502f277483d
Taebearxd <149920040+Taebearxd@users.noreply.github.com>
2025-01-02 09:16:24 +0530

    Update gp-tagall.js

--- plugins/gp-tagall.js
+++ plugins/gp-tagall.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, participants, isAdmin, isOwner, groupMetadata }) => {
     let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
-    m.reply(`▢ Group : *${groupMetadata.subject}*\n▢ Members : *${participants.length}*${text ? `\n▢ Message : ${text}\n` : ''}\n┌───⊷ *MENTIONS*\n` + users.map(v => '▢ @' + v.replace(/@.+/, '')).join`\n` + '\n└──✪ XLICON┃ ᴮᴼᵀ ✪──', null, {
+    m.reply(`▢ Group : *${groupMetadata.subject}*\n▢ Members : *${participants.length}*${text ? `\n▢ Message : ${text}\n` : ''}\n┌───⊷ *MENTIONS*\n` + users.map(v => '𖹭ׅׅ۪۪۪۪ ֺ🌸̷̸̶̼᪲ :: @' + v.replace(/@.+/, '')).join`\n` + '\n︵ᅟ ׅ `⎯⎯ּ︭.`   ⏜֗ ̮   ׄ ׅ🌟𝂻ᩧ̸̷ᮬ ׅׄ ݊ 𐔌. ⎯⎯ּ︭.', null, {
         mentions: users
     })
 }
